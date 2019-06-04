b34e9c674571e0ac2013036cdbb834c6b202f5db
godchiken <funnic2@gmail.com>
2019-06-04 13:34:20 +0900

    기본 설정 진행 중 - logback log direct to elasticsearch 구현분   - appender를 통한 로그 전송 구현

--- src/main/java/com/kbh/elk/app/ELKApplication.java
+++ src/main/java/com/kbh/elk/app/ELKApplication.java
@@ -2,8 +2,10 @@ package com.kbh.elk.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class ELKApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ELKApplication.class, args);
