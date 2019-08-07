205aae76074b9c449663ba16a72694636f961f3c
김보훈 <funnic2@gmail.com>
2019-08-07 20:46:03 +0900

    local and company db property settings

--- src/main/resources/initial_data_query.sql
+++ src/main/resources/initial_data_query.sql
@@ -1,18 +1,38 @@
-INSERT INTO `elk_develop`.`book_store`(`book_store_idx`, `name`) VALUES (1, '김보훈 책방');
-INSERT INTO `elk_develop`.`book_store`(`book_store_idx`, `name`) VALUES (2, '배창현 책방');
-INSERT INTO `elk_develop`.`book_store`(`book_store_idx`, `name`) VALUES (3, '안정훈 책방');
-INSERT INTO `elk_develop`.`book_store`(`book_store_idx`, `name`) VALUES (4, '황성인 책방');
-INSERT INTO `elk_develop`.`book_store`(`book_store_idx`, `name`) VALUES (5, '김동훈 책방');
-INSERT INTO `elk_develop`.`book_store`(`book_store_idx`, `name`) VALUES (6, '정현일 책방');
-INSERT INTO `elk_develop`.`book_store`(`book_store_idx`, `name`) VALUES (7, '왕선호 책방');
-INSERT INTO `elk_develop`.`book_store`(`book_store_idx`, `name`) VALUES (8, '권성봉 책방');
+-- company
 
-INSERT INTO `elk_develop`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (1, '김보훈', '한울네트웍스에서의 생활', 1);
-INSERT INTO `elk_develop`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (2, '배창현', '프레시코드의 바나나 비즈니스 매너', 2);
-INSERT INTO `elk_develop`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (3, '안정훈', '아이나비에서의 혈투', 3);
-INSERT INTO `elk_develop`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (4, '황성인', '패스트캠퍼스에서 생존법', 4);
-INSERT INTO `elk_develop`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (5, '김동훈', '카카오톡 합격하는 법', 5);
-INSERT INTO `elk_develop`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (6, '정현일', 'NTS에서 독고다이', 6);
-INSERT INTO `elk_develop`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (7, '왕선호', 'AWS와 NBP의 차이', 7);
-INSERT INTO `elk_develop`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (8, '권성봉', '프리랜서 유남생', 8);
+-- INSERT INTO `elk_develop`.`book_store`(`book_store_idx`, `name`) VALUES (1, '김보훈 책방');
+-- INSERT INTO `elk_develop`.`book_store`(`book_store_idx`, `name`) VALUES (2, '배창현 책방');
+-- INSERT INTO `elk_develop`.`book_store`(`book_store_idx`, `name`) VALUES (3, '안정훈 책방');
+-- INSERT INTO `elk_develop`.`book_store`(`book_store_idx`, `name`) VALUES (4, '황성인 책방');
+-- INSERT INTO `elk_develop`.`book_store`(`book_store_idx`, `name`) VALUES (5, '김동훈 책방');
+-- INSERT INTO `elk_develop`.`book_store`(`book_store_idx`, `name`) VALUES (6, '정현일 책방');
+-- INSERT INTO `elk_develop`.`book_store`(`book_store_idx`, `name`) VALUES (7, '왕선호 책방');
+-- INSERT INTO `elk_develop`.`book_store`(`book_store_idx`, `name`) VALUES (8, '권성봉 책방');
+--
+-- INSERT INTO `elk_develop`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (1, '김보훈', '한울네트웍스에서의 생활', 1);
+-- INSERT INTO `elk_develop`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (2, '배창현', '프레시코드의 바나나 비즈니스 매너', 2);
+-- INSERT INTO `elk_develop`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (3, '안정훈', '아이나비에서의 혈투', 3);
+-- INSERT INTO `elk_develop`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (4, '황성인', '패스트캠퍼스에서 생존법', 4);
+-- INSERT INTO `elk_develop`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (5, '김동훈', '카카오톡 합격하는 법', 5);
+-- INSERT INTO `elk_develop`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (6, '정현일', 'NTS에서 독고다이', 6);
+-- INSERT INTO `elk_develop`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (7, '왕선호', 'AWS와 NBP의 차이', 7);
+-- INSERT INTO `elk_develop`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (8, '권성봉', '프리랜서 유남생', 8);
 
+-- Local
+INSERT INTO `elk`.`book_store`(`book_store_idx`, `name`) VALUES (1, '김보훈 책방');
+INSERT INTO `elk`.`book_store`(`book_store_idx`, `name`) VALUES (2, '배창현 책방');
+INSERT INTO `elk`.`book_store`(`book_store_idx`, `name`) VALUES (3, '안정훈 책방');
+INSERT INTO `elk`.`book_store`(`book_store_idx`, `name`) VALUES (4, '황성인 책방');
+INSERT INTO `elk`.`book_store`(`book_store_idx`, `name`) VALUES (5, '김동훈 책방');
+INSERT INTO `elk`.`book_store`(`book_store_idx`, `name`) VALUES (6, '정현일 책방');
+INSERT INTO `elk`.`book_store`(`book_store_idx`, `name`) VALUES (7, '왕선호 책방');
+INSERT INTO `elk`.`book_store`(`book_store_idx`, `name`) VALUES (8, '권성봉 책방');
+
+INSERT INTO `elk`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (1, '김보훈', '한울네트웍스에서의 생활', 1);
+INSERT INTO `elk`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (2, '배창현', '프레시코드의 바나나 비즈니스 매너', 2);
+INSERT INTO `elk`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (3, '안정훈', '아이나비에서의 혈투', 3);
+INSERT INTO `elk`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (4, '황성인', '패스트캠퍼스에서 생존법', 4);
+INSERT INTO `elk`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (5, '김동훈', '카카오톡 합격하는 법', 5);
+INSERT INTO `elk`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (6, '정현일', 'NTS에서 독고다이', 6);
+INSERT INTO `elk`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (7, '왕선호', 'AWS와 NBP의 차이', 7);
+INSERT INTO `elk`.`book`(`book_idx`, `author`, `name`, `book_store_idx`) VALUES (8, '권성봉', '프리랜서 유남생', 8);
